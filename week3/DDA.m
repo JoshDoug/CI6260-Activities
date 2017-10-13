1cbda64836accbad07215295cd85a8dd4886133f
Joshua Stringfellow <joshdougstringfellow@gmail.com>
2017-10-14 00:18:27 +0100

    Update week3 activity

--- week3/DDA.m
+++ week3/DDA.m
@@ -1,12 +1,24 @@
-function DDA (x0, y0, x1, y1)
+function DDA( x0, y0, x1, y1 )
+%DDA Finds optimum raster locations to represent a line
 
 dy = y1 - y0;
 dx = x1 - x0;
 m = dy/dx;
 py = y0;
+px = x0;
 
-for px=x0:x1
-  plot(px, round(py), 'ro');
-  py = py + m;
-  hold on
+if (abs(m) < 1) % Plot points where slope is less than 1
+    for px=x0:x1
+        plot(px, round(py), 'ro');
+        py = py + m;
+        hold on
+    end
+elseif (abs(m) > 1) % Plot points where slope is greater than 1
+    for py=y0:y1
+        plot(round(px), py, 'ro');
+        px = px + 1/m;
+        hold on
+    end
+end
+
 end
--- week3/week3.m
+++ week3/week3.m
@@ -1,1 +1,13 @@
-% Week 3 Activityfigure(1)DDA(2,3, 6, 11)
+% Week 3 Workshop Activity
+
+% Using DDA
+figure(1)
+DDA(5, 3, 17, 7) % Plot line with slope between -1 and 1
+DDA(5, 3, 7, 17) % Plot line with slope greater than 1
+axis([0 20 0 20])
+
+% Using Bresenham
+figure(2)
+Bresenham(5, 3, 17, 7) % Plot line with slope between -1 and 1
+Bresenham(5, 3, 7, 17) % Plot line with slope greater than 1
+axis([0 20 0 20])
