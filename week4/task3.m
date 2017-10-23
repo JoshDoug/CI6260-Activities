9ea15e24e20c91a3a8a5acba4bcac23bf2729c6c
Joshua Stringfellow <joshdougstringfellow@gmail.com>
2017-10-23 14:45:00 +0100

    Update week4 task3

--- week4/task3.m
+++ week4/task3.m
@@ -1,13 +1,13 @@
 % Week 4: Task 3 - Malformed Circle
 
-% Dunno what to do here.
+T2 = [1.5 0; 0 0.75];
 
-T2 = [1.5 0 0; 0 0.75 0; 0 0 1];
-
-circle = circleJDS(3, 3, 2, 5);
+circle = circleJDS(0, 0, 2, 50);
 
 line(circle(1,:), circle(2,:)); % Plot original circle
 
-%circle = circle * T2
+circle = T2 * circle;
+
+line(circle(1,:), circle(2,:)); % Plot transformed circle
 
 axis([-10 10 -10 10])
