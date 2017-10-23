fee0257e66fd2cd54d6e46796a6a89768255c7a7
Joshua Stringfellow <joshdougstringfellow@gmail.com>
2017-10-23 14:45:27 +0100

    Update conditional example

--- learningOctave/conditionals.m
+++ learningOctave/conditionals.m
@@ -1,7 +1,11 @@
 % Using conditionals
 
-num = 500
+num = 600
 
 if (num >= 500)
   printf('Num is greater than or equal to 500.\n');
+elseif (num >= 250)
+  printf('Num is greater than or equal to 250.\n');
+else
+  printf('Num is smaller than 250.\n');
 endif
